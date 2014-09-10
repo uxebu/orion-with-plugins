3468c8ed2afc08d905a7dc9687980bfedb26746d
Georg Lang <georglang0788@gmx.net>
2014-09-10 14:41:21 +0200

    added chunkLimitCount Plugin, css loaders, aliases, i18n

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,27 @@
+var I18nPlugin = require('i18n-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
+
+var options = {
+  maxChunks: 1
+}
+
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: './dist/bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin(options),
+    new I18nPlugin(require("null"))
+  ],
+  loaders: [
+    { test: /\.css$/, loaders: ["style", "css"] }
+  ],
+  resolve: {
+    alias: {
+      orion: path.join(__dirname, 'orion'),
+      webtools: path.join(__dirname, 'webtools')
+    }
   }
 };
